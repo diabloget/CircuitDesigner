928fa59a03a2f538c2fd3d29411a6bb61e35bd23
David R (Get) <davidroblesvargasdrv@gmail.com>
2019-09-26 20:03:06 -0600

    Imágenes
    Cambio de ubicación imágenes

--- CD/src/app/Icon.java
+++ CD/src/app/Icon.java
@@ -24,55 +24,55 @@ public class Icon {
      */
     public void setImage(Icon Icon, IconEnum Type) throws FileNotFoundException {
         if(Type == IconEnum.AND) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\And.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\And.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.NAND) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Nand.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Nand.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.NOR) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Nor.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Nor.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.NOT) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Not.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Not.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.OR) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Or.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Or.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.XNOR) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Xnor.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Xnor.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.XOR) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Xor.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Xor.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.SETTINGS) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Settings.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Settings.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.RESTART) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Restart.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Restart.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.OPEN) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Open.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Open.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.SAVE) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Save.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Save.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.RUN) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Run.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Run.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }else if(Type == IconEnum.HELP) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Help.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Help.png");
             Image IconGeneric = new Image(inputFileN);
             this.Icon = IconGeneric;
         }
@@ -92,67 +92,67 @@ public class Icon {
      */
     public void setImageView(Icon ImageView, IconEnum Type) throws FileNotFoundException {
         if(Type == IconEnum.AND) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\And.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\And.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.NAND) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Nand.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Nand.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.NOR) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Nor.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Nor.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.NOT) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Not.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Not.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.OR) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Or.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Or.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.XNOR) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Xnor.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Xnor.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.XOR) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gatesIcon\\Xor.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gatesIcon\\Xor.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.SETTINGS) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Settings.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Settings.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.RESTART) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Restart.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Restart.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.OPEN) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Open.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Open.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.SAVE) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Save.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Save.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.RUN) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Run.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Run.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
         }else if(Type == IconEnum.HELP) {
-            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CD\\src\\gIcon\\Help.png");
+            FileInputStream inputFileN = new FileInputStream("C:\\Users\\diabl\\IdeaProjects\\CircuitDesigner\\CD\\src\\gIcon\\Help.png");
             Image IconGeneric = new Image(inputFileN);
             ImageView IconViewGeneric = new ImageView(IconGeneric);
             this.IconView = IconViewGeneric;
